0fa166fd14b6589eb015a4a5861c9eeca80b9ac4
UXDynamicSolution <cah.munoz.ti@gmail.com>
2021-04-16 19:09:34 -0400

    carga inicial

---
+++ dias.py
@@ -0,0 +1,31 @@
+import math
+
+def descomponer_anios(total_dias):
+	PROMEDIO_ANNIO = 365.20
+	PROMEDIO_MES   = 30.42
+
+	calcular_annios = math.floor(total_dias / PROMEDIO_ANNIO)
+	return round(calcular_annios)
+
+def descomponer_meses(total_dias):
+	PROMEDIO_ANNIO = 365.20
+	PROMEDIO_MES   = 30.42
+
+	calcular_meses = math.floor(total_dias - descomponer_anios(total_dias) * PROMEDIO_ANNIO)
+	meses = math.floor(calcular_meses / PROMEDIO_MES)
+	return (meses)
+
+def descomponer_dias(total_dias):
+	PROMEDIO_ANNIO = 365.20
+	PROMEDIO_MES   = 30.42
+
+	calcular_dias = (total_dias-(descomponer_anios(total_dias) * PROMEDIO_ANNIO + descomponer_meses(total_dias) * PROMEDIO_MES))
+	return math.floor(calcular_dias)
+
+print(str(descomponer_anios(123)))
+print((descomponer_meses(123)))
+print(str(descomponer_dias(123)))
+
+
+	
+
